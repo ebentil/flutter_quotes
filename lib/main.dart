1b3bc319d16e8a5cc951065396eefe8a9d1f4ddf
Ebo <bentilebo@gmail.com>
2020-09-28 21:08:24 +0000

    #20 how to extract a widget

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,34 @@ class _QuoteListState extends State<QuoteList> {
   ];
 
   Widget quoteTemplate(quote) {
+    return QuoteCard(quote: quote);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.grey[300],
+        appBar: AppBar(
+          title: Text('Awesome Quotes'),
+          centerTitle: true,
+          backgroundColor: Colors.redAccent,
+        ),
+        body: Column(
+          children: quotes.map((quote) => quoteTemplate(quote)).toList(),
+
+          // children: quotes.map((quote) {
+          //   return Text(quote);
+          // }).toList(),
+        ));
+  }
+}
+
+class QuoteCard extends StatelessWidget {
+  final Quote quote;
+  QuoteCard({this.quote});
+
+  @override
+  Widget build(BuildContext context) {
     return Card(
       margin: EdgeInsets.fromLTRB(16, 16, 16, 0),
       child: Padding(
@@ -52,23 +80,5 @@ class _QuoteListState extends State<QuoteList> {
         ),
       ),
     );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.grey[300],
-        appBar: AppBar(
-          title: Text('Awesome Quotes'),
-          centerTitle: true,
-          backgroundColor: Colors.redAccent,
-        ),
-        body: Column(
-          children: quotes.map((quote) => quoteTemplate(quote)).toList(),
-
-          // children: quotes.map((quote) {
-          //   return Text(quote);
-          // }).toList(),
-        ));
   }
 }
